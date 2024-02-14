12271dc20ff7dea7d7d8afe104c45b26838f20b8
sosiday22 <sosiday22@gmail.com>
2024-02-15 00:31:35 +0800

    'init'

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
